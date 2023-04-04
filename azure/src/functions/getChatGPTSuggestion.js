85640c69f407fc7968fd5d03ff93d07be847f9d8
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-04-04 11:43:42 +0200

    Added and deployed all functions

--- azure/src/functions/getChatGPTSuggestion.js
+++ azure/src/functions/getChatGPTSuggestion.js
@@ -7,7 +7,7 @@ app.http("getChatGPTSuggestion", {
     handler: async (request, context) => {
         const response = await openai.createCompletion({
             model: "text-davinci-003",
-            prompt: "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
+            prompt: "Write a random text prompt for DALL·E to generate an image, this prompt will be shown to the user, include details such as the genre and what type of painting it should be, options can include: aesthetic, oil painting, watercolor, photo-realistic, 4k, abstract, modern, black and white etc. Do not wrap the answer in quotes.",
             max_tokens: 100,
             temperature: 0.8,
         });
