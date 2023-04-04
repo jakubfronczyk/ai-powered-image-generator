a35c9eb54c4191be41e92ee3f9841e85292f77ed
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-04-04 11:43:19 +0200

    Added all endpoints

---
+++ app/api/generateImages/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+    const res = await request.json(); // res now contains body
+    const prompt = res.prompt;
+
+    const response = await fetch(
+        "https://ai-powered-image-generator-app.azurewebsites.net/api/generateimage",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ prompt }),
+        }
+    );
+
+    const textData = await response.text();
+
+    return NextResponse.json(textData);
+}
--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,27 +1,15 @@
 export async function GET(request: Request) {
-    try {
-        // Make an HTTP GET request to the Azure Function endpoint
-        const response = await fetch(
-            "http://localhost:7071/api/getChatGPTSuggestion",
-            {
-                cache: "no-store",
-            }
-        );
+    // Make an HTTP GET request to the Azure Function endpoint
+    const response = await fetch(
+        "https://ai-powered-image-generator-app.azurewebsites.net/api/getchatgptsuggestion",
+        {
+            cache: "no-store",
+        }
+    );
 
-        // Retrieve the response data
-        const responseData = await response.text();
+    const textData = await response.text();
 
-        // Return the response data as plain text
-        return new Response(responseData.trim(), {
-            status: 200,
-            headers: {
-                "Content-Type": "text/plain",
-            },
-        });
-    } catch (err: any) {
-        console.error(JSON.stringify(err));
-        return new Response(JSON.stringify(err.message), {
-            status: 500,
-        });
-    }
+    return new Response(JSON.stringify(textData.trim()), {
+        status: 200,
+    });
 }
